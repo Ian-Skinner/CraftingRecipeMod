a669a93f8b9e91cff14af3c041875b11e4688ffe
egrojj146 <ryanmarshall050@gmail.com>
2023-12-11 18:10:04 -0500

    added chain mail armor and a base block example

---
+++ forge-1.20.4-49.0.3-mdk/src/main/java/net/modders/crafting_recipe_mod/block/powereddispencer.java
@@ -0,0 +1,14 @@
+package net.modders.crafting_recipe_mod.block;
+
+import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.DispenserBlockEntity;
+
+public class powereddispencer extends DispenserBlock {
+
+    public powereddispencer(Properties properties) {
+        super(properties);
+    }
+
+    
+}
